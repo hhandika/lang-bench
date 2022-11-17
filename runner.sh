2b13f535e594c8e7b707c142e73f21d3e6cda065
hhandika <herubiolog@gmail.com>
2022-11-16 19:54:34 -0600

    Add go and rust.

---
+++ runner.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+set C_OUT "main-c"
+set C_OUT_OPT "main_opt"
+set RS_OUT "main-rs"
+set GO_OUT "main-go"
+
+# Compile C code
+gcc -o $C_OUT main.c
+
+# Compile C code with O3 optimization
+gcc -o $C_OUT_OPT main.c -O3
+
+# Compile Rust code
+rustc -o $RS_OUT main.rs
+
+# Compile Go code
+go build -o $GO_OUT main.go
+
+# Clean cache
+hyperfine --warmup 3 "./$C_OUT" "./$C_OUT_OPT" "./$RS_OUT" "./$GO_OUT" "python main.py"
+
+# Run benchmark
+hyperfine "./$C_OUT" "./$C_OUT_OPT" "./$RS_OUT" "./$GO_OUT" "python main.py"
+
+
+# Clean up
+rm $C_OUT $C_OUT_OPT $RS_OUT
