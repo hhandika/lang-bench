89cbbd5dfd7a91c51a08a432bf42defb7d708faf
hhandika <herubiolog@gmail.com>
2022-11-17 15:27:23 -0600

    Add cpp code.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 0; i < 100000; i++)
+    {
+        std::cout << "Counts:" << i << std::endl;
+    }
+}
--- runner.sh
+++ runner.sh
@@ -2,6 +2,7 @@
 
 set C_OUT "main-c"
 set C_OUT_OPT "main_opt"
+set CPP_OUT "main-cpp"
 set RS_OUT "main-rs"
 set GO_OUT "main-go"
 
@@ -11,6 +12,9 @@ gcc -o $C_OUT main.c
 # Compile C code with O3 optimization
 gcc -o $C_OUT_OPT main.c -O3
 
+# Compile C++ code
+g++ -o $CPP_OUT main.cpp
+
 # Compile Rust code
 rustc -o $RS_OUT main.rs
 
@@ -18,10 +22,10 @@ rustc -o $RS_OUT main.rs
 go build -o $GO_OUT main.go
 
 # Clean cache
-hyperfine --warmup 3 "./$C_OUT" "./$C_OUT_OPT" "./$RS_OUT" "./$GO_OUT" "python main.py"
+hyperfine --warmup 3 "./$C_OUT" "./$C_OUT_OPT" "./$CPP_OUT" "./$RS_OUT" "./$GO_OUT" "python main.py"
 
 # Run benchmark
-hyperfine "./$C_OUT" "./$C_OUT_OPT" "./$RS_OUT" "./$GO_OUT" "python main.py"
+hyperfine "./$C_OUT" "./$C_OUT_OPT" "./$CPP_OUT" "./$RS_OUT" "./$GO_OUT" "python main.py"
 
 
 # Clean up
